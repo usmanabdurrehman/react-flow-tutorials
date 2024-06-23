78379e7441b2247397019db8b1339a2c40456289
Usman Abdur Rehman <saidskit@outlook.com>
2024-06-23 20:07:35 +0500

    Initialized Project

--- src/SchemaVisualizer/SchemaVisualizer.constants.ts
+++ src/SchemaVisualizer/SchemaVisualizer.constants.ts
@@ -1,9 +1,4 @@
-import { Edge, Node } from "reactflow";
-import { Model } from "./SchemaVisualizer.types";
-import { getInfoFromSchema } from "./SchemaVisualizer.utils";
-
-const schema = `
-
+export const schema = `
 model Post{
   id: number;
   title: string;
@@ -24,48 +19,3 @@ model User{
 }
 
 `;
-
-let row = 0;
-let column = 0;
-
-const { models, connections } = getInfoFromSchema(schema);
-
-export const initialEdges: Edge[] = connections.map((connection) => {
-  const sourceId = `${connection.source}-${connection.name}`;
-  return {
-    id: sourceId,
-    source: connection.source,
-    target: connection.target,
-    targetHandle: connection.target,
-    sourceHandle: sourceId,
-    animated: true,
-  };
-});
-
-const numModels = models.length + 1;
-let numGrid = 1;
-while (1) {
-  if (numGrid ** 2 >= numModels) {
-    break;
-  }
-  numGrid += 1;
-}
-
-export const initialNodes: Node<Model>[] = models.map((model, index) => {
-  const x = row * 300;
-  const y = column * 300;
-
-  if (numGrid % index === 0) {
-    column = 0;
-    row += 1;
-  } else {
-    column += 1;
-  }
-
-  return {
-    id: model.name,
-    position: { x, y },
-    type: "model",
-    data: model,
-  };
-});
--- src/SchemaVisualizer/SchemaVisualizer.tsx
+++ src/SchemaVisualizer/SchemaVisualizer.tsx
@@ -1,28 +1,11 @@
 import ReactFlow, { Background, BackgroundVariant, Controls } from "reactflow";
 import "reactflow/dist/style.css";
 import { Box } from "@chakra-ui/react";
-import { initialEdges, initialNodes } from "./SchemaVisualizer.constants";
-import ModelNode from "./ModelNode";
-
-const nodeTypes = {
-  model: ModelNode,
-};
 
 export const SchemaVisualizer = () => {
   return (
-    <Box height={"100vh"} width="100wh" bg="#1C1C1C" pos="relative">
-      <ReactFlow
-        defaultNodes={initialNodes}
-        defaultEdges={initialEdges}
-        nodeTypes={nodeTypes}
-        fitView
-        fitViewOptions={{
-          padding: 0.4,
-        }}
-      >
-        <Background color="#222" variant={BackgroundVariant.Lines} />
-        <Controls />
-      </ReactFlow>
+    <Box height={"100vh"} width="100wh">
+      <ReactFlow></ReactFlow>
     </Box>
   );
 };
